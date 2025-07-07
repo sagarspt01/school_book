988e2c53de32276d78a1c1957a42b4b658a41ad3
sagarspt01 <satapathysagar56@gmail.com>
2025-07-07 20:26:17 +0530

    backend created using django

---
+++ student_book_bend/student_book_bend/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for student_book_bend project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'student_book_bend.settings')
+
+application = get_asgi_application()
